1cad5d90cdf74989567060fe08ed7e7e970326ea
YHJ <yhj2263>
2017-09-29 17:26:44 -0700

    corrected the python source route

--- src/app.js
+++ src/app.js
@@ -115,7 +115,7 @@ app.post('/start_train', function(req, res) {
   // train the classifier
   let modelName = req.body.modelName;
   let classifierName = req.body.classifierName;
-  const childTrain = exec('python src/classifier.py TRAIN'
+  const childTrain = exec('python /root/Facenet/src/classifier.py TRAIN'
   + '/root/Facenet-Server/data/train_data/'
   + '/root/model/' + modelName + '/' + modelName + '.pb'
   + '/root/Facenet-Server/data/model/' + classifierName + '.pkl'
