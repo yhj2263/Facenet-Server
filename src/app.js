7c0da0535e6265c56ca5171bc3c8afca182b7d34
YHJ <yhj2263>
2017-09-29 18:10:20 -0700

    unmuted warnings

--- src/app.js
+++ src/app.js
@@ -115,7 +115,7 @@ app.post('/start_train', function(req, res) {
   // train the classifier
   let modelName = req.body.modelName;
   let classifierName = req.body.classifierName;
-  const childTrain = exec('python -W ignore '
+  const childTrain = exec('python '
   + '/root/Facenet/src/classifier.py TRAIN '
   + '/root/Facenet-Server/data/train_data/ '
   + '/root/model/' + modelName + '/' + modelName + '.pb '
@@ -144,7 +144,7 @@ app.post('/start_classify', function(req, res) {
   // classifying with the specified classifier
   let modelName = req.body.modelName;
   let classifierName = req.body.classifierName;
-  const childTrain = exec('python - W ignore '
+  const childTrain = exec('python '
   + '/root/Facenet/src/classifier.py CLASSIFY '
   + '/root/Facenet-Server/data/test_data/ '
   + '/root/model/' + modelName + '/' + modelName + '.pb '
